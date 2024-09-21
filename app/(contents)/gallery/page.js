d35cc1330d740c3713a964fb56d9e7c3d3bf80f0
evanalifian <evanalifian@gmal.com>
2024-09-21 13:53:29 +0700

    update

--- app/(contents)/gallery/page.js
+++ app/(contents)/gallery/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import dataImages from "@/data.json";
 import Image from "next/image";
 
-export default function page() {
+export default async function page() {
   const firstSideA = dataImages.filter((d, i) => i <= 7);
   const firstSideB = dataImages.filter((d, i) => i > 7);
 
@@ -16,42 +16,42 @@ export default function page() {
       <ul className="flex flex-col gap-4 lg:hidden">
         {firstSideA.map((i) => (
           <li key={i.alt}>
-            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" />
+            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" priority />
           </li>
         ))}
       </ul>
       <ul className="flex flex-col gap-4 lg:hidden">
         {firstSideB.map((i) => (
           <li key={i.alt}>
-            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" />
+            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" priority />
           </li>
         ))}
       </ul>
       <ul className="hidden flex-col gap-4 lg:flex">
         {secondSideA.map((i) => (
           <li key={i.alt}>
-            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" />
+            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" priority />
           </li>
         ))}
       </ul>
       <ul className="hidden flex-col gap-4 lg:flex">
         {secondSideB.map((i) => (
           <li key={i.alt}>
-            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" />
+            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" priority />
           </li>
         ))}
       </ul>
       <ul className="hidden flex-col gap-4 lg:flex">
         {secondSideC.map((i) => (
           <li key={i.alt}>
-            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" />
+            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" priority />
           </li>
         ))}
       </ul>
       <ul className="hidden flex-col gap-4 lg:flex">
         {secondSideD.map((i) => (
           <li key={i.alt}>
-            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" />
+            <Image src={i.image} alt={i.alt} width={500} height={500} className="w-full" priority />
           </li>
         ))}
       </ul>
